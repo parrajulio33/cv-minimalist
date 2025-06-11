2ee9f75e5f2dadfb2d39cdbd913aee7a09e3fb6c
Julio Parra <parrajulio33@gmail.com>
2025-06-11 07:36:55 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,12 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  server: {
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    }
+  },
   site: "https://parrajulio33.github.io/cv-minimalist/",
   base: "/cv-minimalist/",
-  vite: {
-       build: {
-          rollupOptions: {
-             output: {
-                entryFileNames: '[name]-[hash].js',
-             },
-          },
-       },
-    },
 });
