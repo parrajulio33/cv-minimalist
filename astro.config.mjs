eaac6eb0a51bbd30a9f8ce0f68144b3a8b1666cf
Julio Parra <parrajulio33@gmail.com>
2025-06-11 08:58:11 -0600

    add netlify toml file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import netlify from '@astrojs/netlify';
 // https://astro.build/config
 export default defineConfig({
   site: "https://parrajulio33.github.io/cv-minimalist/",
   base: "/cv-minimalist/",
-  adapter: netlify(),	
 });
