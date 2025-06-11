7b6faf4dd888f5fc4e0f347916d0dfded66c21df
Julio Parra <parrajulio33@gmail.com>
2025-06-11 07:51:53 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    headers: {
-      "Access-Control-Allow-Origin": "*"
-    }
-  },
   site: "https://parrajulio33.github.io/cv-minimalist/",
   base: "/cv-minimalist/",
+  output: 'server',
+       server: {
+         headers: {
+           'Referrer-Policy': 'strict-origin-when-cross-origin',
+         },
+       },
+  adapter: netlify(),
 });
