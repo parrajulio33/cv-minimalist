dfd48da180fa2bfa8a929130e839b79a302a6576
Julio Parra <parrajulio33@gmail.com>
2025-06-11 11:50:25 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://parrajulio33.github.io/cv-minimalist/",
-  base: "/cv-minimalist/",
+  base: "/",
+  build: {
+    format: 'directory'
+  }
 });
